9c38f017e16474afcdca0367447660005ed5ccbe
Mohammad Fayaz <fayazfz07@gmail.com>
2022-08-21 16:41:20 +0530

    add logic for pulling app-settings from properties file

--- src/core/config/settings.go
+++ src/core/config/settings.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	appTypes "go-rest-starter/src/core/types"
+	"log"
+	"path/filepath"
+
+	"github.com/magiconair/properties"
+)
+
+var appSettings appTypes.AppSettings
+
+const settingsFileName = "settings.properties"
+
+func init() {
+	loadSettings()
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadSettings() {
+	absolutePath, err := filepath.Abs(settingsFileName)
+	check(err)
+
+	p := properties.MustLoadFile(absolutePath, properties.UTF8)
+	if err := p.Decode(&appSettings); err != nil {
+		log.Panic(err)
+	}
+	log.Println("App settings loaded from " + settingsFileName)
+}
+
+func GetAppSettings() appTypes.AppSettings {
+	return appSettings
+}
